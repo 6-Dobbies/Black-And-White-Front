1fe95d254c0f51718e2f1f32f54aa5d85c1e32c6
mggr27 <84060918+mggr27@users.noreply.github.com>
2021-12-09 11:36:41 +0900

    [Add] merge leftover

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <template>
+  <Header/>
   <div id="nav" style="color: #3cf281">
+    
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link> |
     <router-link to="/board">Board</router-link>
   </div>
+  <Footer/>
   <router-view/>
 </template>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,32 +1,18 @@
 <template>
-<<<<<<< HEAD
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <!-- <UserRegister msg="Welcome to Your Vue.js App"/> -->
-=======
   <div class="home bawbody">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
->>>>>>> 01518aef721d0db830d0be5060c69eb43fd9fe96
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-<<<<<<< HEAD
-// import UserRegister from '@/components/UserRegister.vue'
-=======
 import HelloWorld from '@/components/HelloWorld.vue'
->>>>>>> 01518aef721d0db830d0be5060c69eb43fd9fe96
 
 export default {
   name: 'Home',
   components: {
-<<<<<<< HEAD
-    // UserRegister,
-=======
     HelloWorld
->>>>>>> 01518aef721d0db830d0be5060c69eb43fd9fe96
   }
 }
 </script>
