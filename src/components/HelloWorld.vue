88b8d0ad83494af49cf3c526b4d2fb46ba966053
GERONIMO <geronimo9595@naver.com>
2021-12-09 15:09:00 +0900

    [Design] mainpage

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,32 +1,31 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="bawrowcenter">
+      <br><br><h1>{{ msg }}</h1>
+      <h5>Enjoy your self:)</h5>
+      <br><br>
+      <h1>6 Dobbies</h1>
+      <ul>
+        <li><a href="https://github.com/ptkeb" target="_blank" rel="noopener">🐻 권대안</a></li>
+        <li><a href="https://github.com/mggr27" target="_blank" rel="noopener">🐉 김아라</a></li>
+        <li><a href="https://github.com/aNnaHmiK" target="_blank" rel="noopener">💩 김한나</a></li>
+        <li><a href="https://github.com/YoojinMa" target="_blank" rel="noopener">🦥 마유진</a></li>
+        <li><a href="https://github.com/minstitia" target="_blank" rel="noopener">🧎🏼‍♂️ 이상민</a></li>
+        <li><a href="https://github.com/chaeljin" target="_blank" rel="noopener">🦮 정은진</a></li>
+      </ul>
+      <br><br>
+      <h1>About</h1>
+      <ul>
+        <li><a href="https://github.com/6-Dobbies" target="_blank" rel="noopener">GitHub</a></li>
+        <li><a href="https://bit.ly/3pK8ehP" target="_blank" rel="noopener">Notion</a></li>
+        <li><a href="" target="_blank" rel="noopener">?????</a></li>
+      </ul>
+      <br><br>
+      <h1>뭘 더 넣지...??</h1>
+      <ul>
+        <li><a href="" target="_blank" rel="noopener">●●●</a></li>
+        <li><a href="" target="_blank" rel="noopener">■■■</a></li>
+        <li><a href="" target="_blank" rel="noopener">▲▲▲</a></li>
+      </ul>
   </div>
 </template>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,12 @@
 <template>
-  <div class="home bawbody">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div>
+  <div class="bawhead">
+    <img alt="Chessboard" src="../assets/chess-background.jpg" style="width:100%">
   </div>
+  <div class="home bawbody">
+    <HelloWorld msg="♟Welcome to Black And White♟"/>
+  </div>
+</div>
 </template>
 
 <script>
@@ -12,7 +16,7 @@ import HelloWorld from '@/components/HelloWorld.vue'
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    HelloWorld, 
   }
 }
 </script>
