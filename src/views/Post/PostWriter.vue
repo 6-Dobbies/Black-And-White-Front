835a369c4ce766b52f7b5a00befd4f4ba2f64508
GERONIMO <geronimo9595@naver.com>
2021-12-21 03:29:59 +0900

    [Design] css

--- src/views/Post/PostWriter.vue
+++ src/views/Post/PostWriter.vue
@@ -1,13 +1,11 @@
 <template>
   <div class="bawhead">
-    <h6>{{name}}님, 환영합니다💜</h6>
+    <!-- <h6>{{name}}님, 환영합니다💜</h6> -->
     <br>
     <h1 style="color: #dee2e6">자유 게시판</h1>
     <hr class="bawline" style="height: 5px;">
   </div>
-  <div>
-    <PostWrite post="글글글"/>
-  </div>
+  <PostWrite post="글글글"/>
 </template>
 
 
